64dd1296c28e7c605b3c18ad748f82be4027f01e
Kenny Turner <59434835+kennyturner@users.noreply.github.com>
2023-05-24 10:20:08 +0100

    Update page-tree-top.blade.php
    Fix a bug when no name available

--- src/views/components/page/page-tree-top.blade.php
+++ src/views/components/page/page-tree-top.blade.php
@@ -7,7 +7,7 @@
     @foreach ($pages as $item)
         <li>
             <a href="{{ $item->home_page ? route('home') : route('page.index', $item->slug) }}"
-                class="underline {{ Route::current()->getName() == $item->slug ? 'active' : '' }}">
+                class="underline {{ optional(Route::current())->getName() == $item->slug ? 'active' : '' }}">
                 {{ $item->menu_title ?? $item->title }}
             </a>
         </li>
