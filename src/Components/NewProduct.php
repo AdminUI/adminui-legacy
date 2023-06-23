f9cb569ea3434c73ffcb9da3c23bd95b4c1f308e
Kenny Turner <59434835+kennyturner@users.noreply.github.com>
2023-06-23 06:55:05 +0100

    Fix non parent products from showing

--- src/Components/NewProduct.php
+++ src/Components/NewProduct.php
@@ -24,7 +24,7 @@ class NewProduct extends Component
     {
         $rows = cache()->remember('newproducts', 3600, function () {
             return Product::where('is_active', 1)
-                ->whereNull('parent_id')
+                ->where('parent_id', 0)
                 ->where('quantity', '>=', 1)
                 ->latest()
                 ->paginate($this->limit);
